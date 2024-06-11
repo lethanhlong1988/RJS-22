c811dd32e1abd774804e512ac39aca509321d541
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-11 04:03:42 +0000

    update

---
+++ frontend/src/util/auth.js
@@ -0,0 +1,32 @@
+import { redirect } from "react-router-dom";
+
+export function getAuthToken() {
+  const token = localStorage.getItem("token");
+  return token;
+}
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
+
+  return null;
+}
+
+// export function checkAuthLoader() {
+//   // this function will be added in the next lecture
+//   // make sure it looks like this in the end
+//   const token = getAuthToken();
+
+//   if (!token) {
+//     return redirect("/auth");
+//   }
+
+//   return null; // this is missing in the next lecture video and should be added by you
+// }
